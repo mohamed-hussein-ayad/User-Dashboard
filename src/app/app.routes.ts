617049c30f0af895d4073217cd645e2ed343b962
mohamed ayad <126675322+mohamed-hussein-ayad@users.noreply.github.com>
2024-08-07 08:54:49 +0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: UserListComponent },
+  { path: 'user/:id', component: UserDetailsComponent },
+];
